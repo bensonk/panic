dc8804fa90bc9c9b4fb63d2c45b3d742a0d7e7e6
Benson Kalahar <bensonk42@gmail.com>
2012-01-15 14:33:49 -0800

    Added empty rails app

---
+++ panic-webapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PanicWebapp::Application.config.secret_token = '648ed2ae46075ac037f83ede9b4c5773b1ae804f04f80280453899757663a3c99d92e80050083088e5e38aeb856bbdd3f0565a2b599bc51fa529b7a010b02109'
